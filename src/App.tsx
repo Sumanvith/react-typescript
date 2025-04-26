ef1339ae1898f93ffe4d896ba47fedefa9b3e7bf
Sumanvith <dellbilla@gmail.com>
2025-04-26 21:38:42 +0530

    Union Type, Function Types, Any Type, unknown and never Type, Aliases ( type and interface ), Extending types, Extending interface, Extending type with interface

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,34 @@ import logo from "./logo.svg";
 import "./App.css";
 
 let name: string;
-let age: number;
+let age: number | string;
 let isStudent: boolean;
 let hobbies: string[];
 let role: [number, string];
-type Person = {
-  name: string;
-  age?: number;
-};
-let person: Person = {
-  name: "hello",
-  age: 3,
+
+function printName(name: string) {
+  console.log(name);
+}
+
+let personName: unknown;
+
+printName("Hello");
+
+type Person1 = {
+  name_type: string;
+  age_type?: number;
 };
 
-let lotsOfPeople: Person[];
+interface Person2 extends Person1 {
+  name_int: string;
+  age_int?: number;
+}
+let person2: Person2 = {
+  name_type: "hello",
+  age_type: 3,
+  name_int: "bye",
+  age_int: 5,
+};
 
 function App() {
   return <div className="App">Hello World</div>;
