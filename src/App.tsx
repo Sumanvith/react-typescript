2f56a34b38c58e6ffe9052a0bb37ef98507dcf78
Sumanvith <dellbilla@gmail.com>
2025-04-29 11:21:06 +0530

    Passing function as props, Event Type in Typescript, Create Todo Logic, useRef Hook with Typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,31 @@
 import React, { useState } from "react";
 import InputField from "./components/InputField";
+import { Todo } from "./model";
 
 const App: React.FC = () => {
   const [todo, setTodo] = useState<string>("");
-
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (todo) {
+      setTodos([...todos, { id: Date.now(), todo, isDone: false }]);
+      setTodo("");
+    }
+  };
+  console.log(todos);
   return (
     <div className="w-screen h-screen flex flex-col items-center bg-[#2f74c0]">
       <span className="uppercase text-[40px] mx-0 my-8 text-white z-[1] text-center">
         Taskify
       </span>
-      <InputField todo={todo} setTodo={setTodo}></InputField>
+      <InputField
+        todo={todo}
+        setTodo={setTodo}
+        handleAdd={handleAdd}></InputField>
+      {/* <TodoList></TodoList> */}
+      {todos.map((t) => (
+        <li>{t.todo}</li>
+      ))}
     </div>
   );
 };
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,19 +1,27 @@
-import React from "react";
+import React, { useRef } from "react";
 
 interface Props {
   todo: string;
   setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (e: React.FormEvent) => void;
 }
 
-const InputField: React.FC<Props> = ({ todo, setTodo }: Props) => {
+const InputField: React.FC<Props> = ({ todo, setTodo, handleAdd }: Props) => {
+  const inputRef = useRef<HTMLInputElement>(null);
   return (
-    <form className="flex w-[90%] relative items-center">
+    <form
+      className="flex w-[90%] relative items-center"
+      onSubmit={(e) => {
+        handleAdd(e);
+        inputRef.current?.blur();
+      }}>
       <input
+        ref={inputRef}
         value={todo}
         onChange={(e) => setTodo(e.target.value)}
         type="text"
         placeholder="Enter a task"
-        className="shadow-2xl text-2xl rounded-[50px] w-[100%] py-5 px-8 transition duration-200"></input>
+        className="text-2xl rounded-[50px] w-[100%] py-5 px-8 transition duration-200 focus:outline-none focus:shadow-[0_10px_1000px_rgba(0,0,0,0.5)]"></input>
       <button
         type="submit"
         className="absolute w-12 h-12 m-3 rounded-full right-0 border-none text-base bg-[#2f74c0] text-white transition-all duration-200 hover:bg-[#388ae2] transform active:scale-90">
