69d1fa387e4c406f1a37229c050415c406c6ee63
Sumanvith <dellbilla@gmail.com>
2025-05-03 21:23:53 +0530

    Delete Todo Functionality, Edit Todo Functionality, Edit Bug Fix

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement } from "react";
+import React, { ReactElement, useEffect, useRef, useState } from "react";
 import { Todo } from "../model";
 import { AiFillDelete, AiFillEdit } from "react-icons/ai";
 import { MdDone } from "react-icons/md";
@@ -10,13 +10,64 @@ type Props = {
 };
 
 const SingleTodo: React.FC<Props> = ({ todo, todos, setTodos }: Props) => {
+  const [edit, setEdit] = useState<boolean>(false);
+  const [editTodo, setEditTodo] = useState<string>(todo.todo);
+
+  const handleDone = (id: number) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isDone: !todo.isDone } : todo
+      )
+    );
+  };
+
+  const handleDelete = (id: number) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  const handleEdit = (e: React.FormEvent, id: number) => {
+    e.preventDefault();
+    setTodos(
+      todos.map((todo) => (todo.id === id ? { ...todo, todo: editTodo } : todo))
+    );
+    setEdit(false);
+  };
+
+  useEffect(() => {
+    inputRef.current?.focus();
+  }, [edit]);
+  const inputRef = useRef<HTMLInputElement>(null);
+
   return (
-    <form className="w-full bg-yellow-400 rounded-md p-4 mt-4 flex items-center justify-between shadow-md">
-      <span className="text-lg">{todo.todo}</span>
+    <form
+      className="w-full bg-yellow-400 rounded-md p-4 mt-4 flex items-center justify-between shadow-md"
+      onSubmit={(e) => handleEdit(e, todo.id)}>
+      {edit ? (
+        <input
+          value={editTodo}
+          onChange={(e) => setEditTodo(e.target.value)}
+          ref={inputRef}></input>
+      ) : todo.isDone ? (
+        <s className="text-lg">{todo.todo}</s>
+      ) : (
+        <span className="text-lg">{todo.todo}</span>
+      )}
+
       <div className="flex gap-3 text-xl">
-        <span>{AiFillEdit({}) as ReactElement}</span>
-        <span>{AiFillDelete({}) as ReactElement}</span>
-        <span>{MdDone({}) as ReactElement}</span>
+        <span
+          onClick={() => {
+            if (!edit && !todo.isDone) {
+              setEdit(!edit);
+            }
+          }}>
+          {AiFillEdit({}) as ReactElement}
+        </span>
+        <span onClick={() => handleDelete(todo.id)}>
+          {AiFillDelete({}) as ReactElement}
+        </span>
+        <span onClick={() => handleDone(todo.id)}>
+          {MdDone({}) as ReactElement}
+        </span>
       </div>
     </form>
   );
